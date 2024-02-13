999834c7601c5790325164b96cb7e5ecc4a2c20b
estefaniari <159870626+estefaniari@users.noreply.github.com>
2024-02-13 01:08:10 -0500

    Agregados archivos

---
+++ Converidor de temperaturas/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace convertidor_de_temperaturas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvertidor = new System.Windows.Forms.Button();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.cmbSeleccion = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTemperatura = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbSeleccion2 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLimpiardatos = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConvertidor
+            // 
+            this.btnConvertidor.BackColor = System.Drawing.Color.Salmon;
+            this.btnConvertidor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvertidor.Location = new System.Drawing.Point(153, 251);
+            this.btnConvertidor.Name = "btnConvertidor";
+            this.btnConvertidor.Size = new System.Drawing.Size(124, 47);
+            this.btnConvertidor.TabIndex = 0;
+            this.btnConvertidor.Text = "Convertir";
+            this.btnConvertidor.UseVisualStyleBackColor = false;
+            this.btnConvertidor.Click += new System.EventHandler(this.btnConvertidor_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(162, 347);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(100, 20);
+            this.txtResultado.TabIndex = 1;
+            // 
+            // cmbSeleccion
+            // 
+            this.cmbSeleccion.FormattingEnabled = true;
+            this.cmbSeleccion.Items.AddRange(new object[] {
+            "Fahrenheit",
+            "Celsius",
+            "Kelvin"});
+            this.cmbSeleccion.Location = new System.Drawing.Point(153, 133);
+            this.cmbSeleccion.Name = "cmbSeleccion";
+            this.cmbSeleccion.Size = new System.Drawing.Size(121, 21);
+            this.cmbSeleccion.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(99, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 27);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Seleccione la temperatura a convertir";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Uighur", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(87, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(270, 35);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Convertidor de temperaturas.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(177, 317);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 27);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Resultado\r\n";
+            // 
+            // txtTemperatura
+            // 
+            this.txtTemperatura.Location = new System.Drawing.Point(162, 225);
+            this.txtTemperatura.Name = "txtTemperatura";
+            this.txtTemperatura.Size = new System.Drawing.Size(100, 20);
+            this.txtTemperatura.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(99, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(227, 27);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Ingrese la temperatura a convertir";
+            // 
+            // cmbSeleccion2
+            // 
+            this.cmbSeleccion2.FormattingEnabled = true;
+            this.cmbSeleccion2.Items.AddRange(new object[] {
+            "Fahrenheit",
+            "Celsius",
+            "Kelvin"});
+            this.cmbSeleccion2.Location = new System.Drawing.Point(153, 160);
+            this.cmbSeleccion2.Name = "cmbSeleccion2";
+            this.cmbSeleccion2.Size = new System.Drawing.Size(121, 21);
+            this.cmbSeleccion2.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::convertidor_de_temperaturas.Properties.Resources._566522_1_;
+            this.pictureBox1.Location = new System.Drawing.Point(329, 274);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(117, 116);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnLimpiardatos
+            // 
+            this.btnLimpiardatos.BackColor = System.Drawing.Color.Gold;
+            this.btnLimpiardatos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiardatos.Location = new System.Drawing.Point(21, 377);
+            this.btnLimpiardatos.Name = "btnLimpiardatos";
+            this.btnLimpiardatos.Size = new System.Drawing.Size(112, 23);
+            this.btnLimpiardatos.TabIndex = 10;
+            this.btnLimpiardatos.Text = "Limpiar Datos";
+            this.btnLimpiardatos.UseVisualStyleBackColor = false;
+            this.btnLimpiardatos.Click += new System.EventHandler(this.btnLimpiardatos_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(458, 402);
+            this.Controls.Add(this.btnLimpiardatos);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cmbSeleccion2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTemperatura);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbSeleccion);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.btnConvertidor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvertidor;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.ComboBox cmbSeleccion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTemperatura;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbSeleccion2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnLimpiardatos;
+    }
+}
+
---
+++ Converidor de temperaturas/Form1.txt
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace convertidor_de_temperaturas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvertidor_Click(object sender, EventArgs e)
+        {
+            {
+                if (string.IsNullOrWhiteSpace(txtTemperatura.Text))
+                {
+                    MessageBox.Show("Ingrese un valor de temperatura.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                double temperatura;
+                if (!double.TryParse(txtTemperatura.Text, out temperatura))
+                {
+                    MessageBox.Show("Ingrese un valor numérico válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                double result;
+                string fromUnit = cmbSeleccion.SelectedItem.ToString();
+                string toUnit = cmbSeleccion2.SelectedItem.ToString();
+
+                if (fromUnit == "Fahrenheit" && toUnit == "Celsius")
+                {
+                    result = (temperatura - 32) * 5 / 9;
+                }
+                else if (fromUnit == "Fahrenheit" && toUnit == "Kelvin")
+                {
+                    result = (temperatura + 459.67) * 5 / 9;
+                }
+                else if (fromUnit == "Celsius" && toUnit == "Fahrenheit")
+                {
+                    result = (temperatura * 9 / 5) + 32;
+                }
+                else if (fromUnit == "Celsius" && toUnit == "Kelvin")
+                {
+                    result = temperatura + 273.15;
+                }
+                else if (fromUnit == "Kelvin" && toUnit == "Fahrenheit")
+                {
+                    result = (temperatura * 9 / 5) - 459.67;
+                }
+                else if (fromUnit == "Kelvin" && toUnit == "Celsius")
+                {
+                    result = temperatura - 273.15;
+                }
+                else
+                {
+                    MessageBox.Show("Seleccione unidades válidas para la conversión.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                txtResultado.Text = result.ToString();
+            }
+        }
+
+        private void btnLimpiardatos_Click(object sender, EventArgs e)
+        {
+            txtTemperatura.Text = string.Empty;
+            txtResultado.Text = string.Empty;
+            cmbSeleccion.Text = string.Empty;
+            cmbSeleccion2.Text = string.Empty;
+        }
+    }
+}
+
